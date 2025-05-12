d26ca0361784013ccee68d792abcd293b47b51a9
Abdul Basit Shahid Deshmukh <ST10316123@vcconnect.edu.za>
2025-05-12 12:24:40 +0200

    InitialCommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROG7311_Part2_POE_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
